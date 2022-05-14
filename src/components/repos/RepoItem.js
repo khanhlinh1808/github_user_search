4072f226f091ea033cf01ec8e49b31702ea3dc2d
Linh Do <khanhlinhdothi@gmail.com>
2022-05-14 21:42:25 +0700

    adding changes v1.0

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,9 +1,8 @@
 import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
-import { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 import atob from "atob";
 
-import githubContext from "../../context/Github/githubContext";
 import getDataApi from "../service/getDataApi";
 import "../../style/UserRepo.scss";
 
@@ -11,42 +10,50 @@ const RepoItem = ({ repo }) => {
   const [popupReadMe, setPopupReadMe] = useState(false);
   const [readMeContent, setReadMeContent] = useState("");
 
-  const handlePopUp = async () => {
-    setPopupReadMe(true);
-    const result = await getDataApi(
-      `https://api.github.com/repos/${login}/${repo.name}/contents/README.md`
-    ).then((response) => setReadMeContent(atob(response.data.content)));
-    console.log(readMeContent);
-  };
   const handleHidePopUp = () => {
     setPopupReadMe(false);
-    console.log("Handle Hide Pop Up");
   };
+  const handlePopUp = async () => {
+    setPopupReadMe(true);
+    await getDataApi(
+      `https://api.github.com/repos/${login}/${repo.name}/contents/README.md`
+    )
+      .then((response) => setReadMeContent(atob(response.data.content)))
+      .catch((err) =>
+        setReadMeContent("There is no README.md file in this repository")
+      );
+  };
+
   const { login } = useParams();
   return (
-    // <div className="repoCardContainer">
-    <div className="repoCard" onClick={handlePopUp}>
-      <h3>
-        <a href={repo.html_url}>{repo.name}</a>
-      </h3>
-      <ul>
-        <li>Forks:{repo.forks}</li>
-        <li>Open Issues:{repo.open_issues}</li>
-        <li>Watchers:{repo.watchers}</li>
-      </ul>
+    <>
+      <div className="repoCard" onClick={handlePopUp}>
+        <h3>
+          <a href={repo.html_url}>{repo.name}</a>
+        </h3>
+        <ul>
+          <li>Forks:{repo.forks}</li>
+          <li>Open Issues:{repo.open_issues}</li>
+          <li>Watchers:{repo.watchers}</li>
+        </ul>
+      </div>
+
       {popupReadMe && (
-        <div className="repoCardPopUp" onClick={handleHidePopUp}>
-          {readMeContent}
-        </div>
+        <>
+          <div className="overlay" onClick={handleHidePopUp} />
+          <div
+            className="repoCardPopUp"
+            id="repoCardPopUp"
+            onClick={handleHidePopUp}
+          >
+            {readMeContent}
+          </div>
+        </>
       )}
-    </div>
-    //   //{" "}
-    // </div>
+    </>
   );
 };
-
 RepoItem.propTypes = {
   repo: PropTypes.object.isRequired,
 };
-
 export default RepoItem;
