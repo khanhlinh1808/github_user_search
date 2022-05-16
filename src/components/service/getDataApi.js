811c73b3beb27af167620e8fd686967ec8d463ec
Linh Do <khanhlinhdothi@gmail.com>
2022-05-16 15:35:19 +0700

    adding changes v1.0

--- src/components/service/getDataApi.js
+++ src/components/service/getDataApi.js
@@ -3,7 +3,7 @@ const API_KEY = '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'
 const getDataApi = async (url) => {
   const res = await axios.get(url, {
     headers: {
-      Authorization: 'Token ' + API_KEY,
+      Authorization: 'Token' + API_KEY,
     },
   })
   return res
